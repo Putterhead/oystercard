42b73776b9fcf3b69a9c44d47f66437c34926a86
PeteGH <peter@grant-hay.eu>
2017-01-10 15:30:00 +0100

    adds touch_in_out(8)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,20 +1,39 @@
+require_relative './balance_error'
+
 class Oystercard
 
-  MAXIMUM_BALANCE = 90
+  BALANCE_LIMIT = 90
 
-  attr_reader :balance
+  attr_reader :balance, :state
 
   def initialize(balance=0)
     @balance = balance
+    @state = false
   end
 
   def top_up(amount)
-    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
+    fail(BalanceError, "Maximum balance of #{BALANCE_LIMIT} exceeded") if over_limit?(amount)
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @state
+  end
+
+  def touch_in
+    @state = true
+  end
+
+  def touch_out
+    @state = false
+  end
+
+  def over_limit?(amount)
+    (@balance + amount) > BALANCE_LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,39 +1,52 @@
 require 'oystercard'
 
+
 describe Oystercard do
   # In order to use public transport
   # As a customer
   # I want money on my card
+  subject(:oystercard){described_class.new}
   it { is_expected.to respond_to :balance }
 
   it 'Shows initial balance of zero' do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
 
   describe '#top_up' do
 
     # it { is_expected.to respond_to(:top_up).with(1).argument }
+  context 'it has a full balance' do
+    before{oystercard.top_up(Oystercard::BALANCE_LIMIT)}
 
-    it 'tops up the card balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-    end
-
-    it 'raises an error if the maximum balance is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    it "won't let you top up over the balance limit" do
+      expect{ oystercard.top_up(1)}.to raise_error(BalanceError, "Maximum balance of #{Oystercard::BALANCE_LIMIT} exceeded")
     end
   end
+end
 
     describe '#deduct' do
     # it { is_expected.to respond_to(:deduct).with(1).argument }
 
       it 'deducts from the card balance' do
-        subject.top_up(20)
-        expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
+        oystercard.top_up(20)
+        expect{ oystercard.deduct 1 }.to change{ oystercard.balance }.by -1
       end
     end
 
+  it 'is initially not in a journey' do
+    expect(oystercard).not_to be_in_journey
+  end
+
+  it 'can touch in' do
+    oystercard.touch_in
+    expect(oystercard).to be_in_journey
+  end
+
+  it 'can touch out' do
+    oystercard.touch_in
+    oystercard.touch_out
+    expect(oystercard).not_to be_in_journey
+  end
     #  it { is_expected.to respond_to(:touch_in)}
 
 end
